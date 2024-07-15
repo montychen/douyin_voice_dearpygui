78ddb77fa4e0f4d983d4808b8134d648ec3318d3
montychen <dj2503@qq.com>
2024-07-15 18:54:02 +0800

    使得窗口window自动匹配Viewport的大小

--- douyin.py
+++ douyin.py
@@ -1,18 +1,18 @@
 
 import dearpygui.dearpygui as dpg
-from utils.reg_font import reg_font
+from utils.reg_font import reg_cnfont
 
 dpg.create_context()
 dpg.create_viewport(title="抖音播音助手:刷礼物、发弹幕、新人进入自动播放语音", width=1152, height=1152)
 
-# 注册中文字体，支持中文，可以自选字体
-reg_font(fontfile_path="font/站酷文艺体.ttf", font_size=24)
+reg_cnfont(font_size=24)    # 注册中文字体，支持中文
 
 
-# label 窗体标签，就是窗体上显示的标签（真啰嗦的话，以后不解释label了）
-# pos 窗体在视窗（viewport)中出现的位置。
-with dpg.window(label='test_dearpygui_hello_world', width=1024, height=800, pos=(20, 20)):
-
+# autosize=True 使得窗口window自动匹配Viewport的大小。
+# no_resize=True 防止用户手动调整window大小，确保窗口始终填充整个Viewport。
+# no_title_bar=True移除了窗口的标题栏，这在某些应用中可能需要，以达到全屏或无边框效果
+with dpg.window(tag="main_window", no_title_bar=True, autosize=True, no_resize=True):
+# with dpg.window(label='test_dearpygui_hello_world', width=1024, height=800, pos=(20, 20)):
     with dpg.group(label='整体组合', horizontal=True, horizontal_spacing=10):  # 把说明文字和输入框放在同一行
         dpg.add_text(label='mytxt', default_value='直播间地址', tag='desc_live_room_url')
         dpg.add_input_text(label='', hint="https开头的完整直播间地址，例如 https://live.douyin.com/896208194652", width=800,  tag='live_room_url')
--- utils/reg_font.py
+++ utils/reg_font.py
@@ -1,10 +1,12 @@
 import dearpygui.dearpygui as dpg
 
-def reg_font(*, fontfile_path: str, font_size: int):  # * 指定后面的参数是关键字参数，如 font_size = 22
+CN_FONT = "font/站酷文艺体.ttf"
+
+def reg_cnfont(*, font_size: int):  # * 指定后面的参数是关键字参数，如 font_size = 22
     # 注册中文字体，支持中文，可以自选字体
     with dpg.font_registry():
-        with dpg.font(fontfile_path, 22) as font1:  # 增加中文编码范围，防止问号
+        with dpg.font(CN_FONT, font_size) as cn_font:  # 增加中文编码范围，防止问号
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Simplified_Common)
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
-        dpg.bind_font(font1)
+        dpg.bind_font(cn_font)   # 最关键的一句
